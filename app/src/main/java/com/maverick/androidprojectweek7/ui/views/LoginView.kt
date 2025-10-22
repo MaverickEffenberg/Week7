351eb688574176aa1a7a02af3f3fab9345596590
MaverickEffenberg <bapak321bapak@gmail.com>
2025-10-22 09:05:29 +0700

    LoginView
    barusan buat login view

---
+++ app/src/main/java/com/maverick/androidprojectweek7/ui/views/LoginView.kt
@@ -0,0 +1,21 @@
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.maverick.androidprojectweek7.ui.theme.AndroidProjectWeek7Theme
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidProjectWeek7Theme {
+        Greeting("Android")
+    }
+}
